3696fa72100f3ab981956faf80079e3ecec79b35
ST10302323 <ST10302323@vcconnect.edu.za>
2024-04-16 14:35:20 +0200

    FifthCommit
    Class was added

---
+++ FifthCommit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ST10302323_POE_Part1
+{
+    internal class Program
+    {
+        class Ingredient
+        {
+            public string Name { get; set; }
+            public double Quantity { get; set; }
+            public string Unit { get; set; }
+        }//Ingredient
+
+        class Step
+        {
+            public string Description { get; set; }
+        }//Step
+
+        class Recipe
+        {
+            public List<Ingredient> Ingredients { get; private set; }
+            public List<Step> Steps { get; private set; }
+
+            public Recipe()
+            {
+                Ingredients = new List<Ingredient>();
+                Steps = new List<Step>();
+            }//Recipe
+
+            public void AddIngredient(string name, double quantity, string unit)
+            {
+                Ingredients.Add(new Ingredient { Name = name, Quantity = quantity, Unit = unit });
+            }//AddIngredient
+
+            public void AddStep(string description)
+            {
+                Steps.Add(new Step { Description = description });
+            }//AddStep
