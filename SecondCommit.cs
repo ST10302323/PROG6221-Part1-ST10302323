8d24236f7c9ebe81a0bedaad60de8c3c3f74ce02
ST10302323 <ST10302323@vcconnect.edu.za>
2024-04-16 14:31:12 +0200

    SecondCommit

---
+++ SecondCommit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ST10302323_POE_Part1
+{
+    internal class Program
+    {
+        class Ingredient
+        {
+            public string Name { get; set; }
+            public double Quantity { get; set; }
+            public string Unit { get; set; }
+        }//Ingredient
+
+        class Step
+        {
+            public string Description { get; set; }
+        }//Step
+
+        class Recipe
+        {
+            public List<Ingredient> Ingredients { get; private set; }
+            public List<Step> Steps { get; private set; }
+
+            public Recipe()
+            {
+                Ingredients = new List<Ingredient>();
+                Steps = new List<Step>();
+            }//Recipe
